02fadaea0e08aa08080f276a765276001cb73719
Dardino <dardino@hotmail.com>
2018-12-17 23:12:13 +0100

    stili css per tabellone

--- src/app/tabellone/tabellone.component.ts
+++ src/app/tabellone/tabellone.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'tdn-tabellone',
-  templateUrl: './tabellone.component.html',
-  styleUrls: ['./tabellone.component.styl']
+  selector: "tdn-tabellone",
+  templateUrl: "./tabellone.component.html",
+  styleUrls: ["./tabellone.component.styl"]
 })
 export class TabelloneComponent implements OnInit {
-
+  Numbers = Array<number>(90).fill(0).map((v, i) => i + 1);
   constructor() { }
 
   ngOnInit() {
